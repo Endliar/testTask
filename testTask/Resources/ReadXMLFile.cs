b6406e6b09b71f049047616c54fd9f56fb0b4fae
endliar <endliar@yandex.ru>
2023-06-18 15:26:43 +0700

    Разделил функционал классов по нормальному.

---
+++ testTask/Resources/ReadXMLFile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using testTask;
+using testTask.Resources;
+
+namespace testTask.Resources
+{
+    public class ReadXMLFile
+    {
+
+        const string primaryXmlFile = "C://Users//endli//source//repos//testTask//testTask//Clients.xml";
+        const string xmlFile = "C://Users//endli//source//repos//testTask//testTask//example.xml";
+        const string newXmlFile = "C://Users//endli//source//repos//testTask//testTask//newExample.xml";
+        const string registratorXmlList = "C://Users//endli//source//repos//testTask//testTask//registratorList.xml";
+
+        public void RemoveMultipleElement()
+            {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlFile);
+
+                XmlNodeList clientNodes = xmlDoc.SelectNodes("//Client");
+
+                foreach (XmlNode clientNode in clientNodes)
+                {
+                    XmlNode removeNode = clientNode.SelectSingleNode("DiasoftID");
+                    if (removeNode != null)
+                    {
+                        string fio = clientNode.SelectSingleNode("FIO").InnerText.Trim();
+                        string regNumber = clientNode.SelectSingleNode("RegNumber").InnerText.Trim();
+                        string diasoftID = clientNode.SelectSingleNode("DiasoftID").InnerText.Trim();
+                        string registrator = clientNode.SelectSingleNode("Registrator").InnerText.Trim();
+
+                        bool isFioValid = !string.IsNullOrEmpty(fio);
+                        bool isRegNumberValid = !string.IsNullOrEmpty(regNumber);
+                        bool isDiasoftIdValid = !string.IsNullOrEmpty(diasoftID);
+                        bool isRegistratorValid = !string.IsNullOrEmpty(registrator);
+
+                        if (!isFioValid || !isRegNumberValid || !isDiasoftIdValid || !isRegistratorValid)
+                        {
+                            xmlDoc.DocumentElement.RemoveChild(clientNode);
+                        }
+                    }
+                }
+                xmlDoc.Save(newXmlFile);
+            }
+
+            public void AutoIncrementID(string xmlFile, string myNode, string reg)
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlFile);
+
+                XmlNodeList clientNodes = xmlDoc.SelectNodes(myNode);
+
+                int id = 1;
+                foreach (XmlNode clientNode in clientNodes)
+                {
+                    XmlAttribute idAttribute = xmlDoc.CreateAttribute(reg);
+                    idAttribute.Value = id.ToString();
+                    clientNode.Attributes.Append(idAttribute);
+                    id++;
+                }
+                xmlDoc.Save(xmlFile);
+            }
+        }
+    }
---
+++ testTask/Resources/RegistratorList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace testTask.Resources
+{
+
+    public class Registrator
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+    }
+
+    public class RegistratorList
+    {
+        const string primaryXmlFile = "C://Users//endli//source//repos//testTask//testTask//Clients.xml";
+        const string xmlFile = "C://Users//endli//source//repos//testTask//testTask//example.xml";
+        const string newXmlFile = "C://Users//endli//source//repos//testTask//testTask//newExample.xml";
+        const string registratorXmlList = "C://Users//endli//source//repos//testTask//testTask//registratorList.xml";
+
+            public void GenerateRegistratorList(string newXmlFile)
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(newXmlFile);
+
+                XmlNodeList clientNodes = xmlDoc.SelectNodes("//Client");
+
+                List<Registrator> registrators = new List<Registrator>();
+
+                foreach (XmlNode clientNode in clientNodes)
+                {
+                    XmlNode registratorNode = clientNode.SelectSingleNode("Registrator");
+                    if (registratorNode != null)
+                    {
+                        string registratorName = registratorNode.InnerText;
+                        string registratorId = clientNode.Attributes["RegistratorID"]?.Value;
+
+                        registrators.Add(new Registrator { Name = registratorName, ID = registratorId });
+                    }
+                }
+
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Registrator>));
+                using (FileStream stream = new FileStream(registratorXmlList, FileMode.Create))
+                {
+                    xmlSerializer.Serialize(stream, registrators);
+                }
+            }
+        
+    }
+}
